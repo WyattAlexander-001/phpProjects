c591bed07494d039ec4283eb10d31b0177d185df
WyattAlexander-001 <wyattbushman117@gmail.com>
2024-03-29 02:20:25 -0400

    Created initial template reference for php
    I haven't had to learn php but it seems useful for freelance work. Made a basic template to refer back to for ref

---
+++ brocode/index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="get">
+        <label for="">Username:</label><br>
+        <input type="text" name="username" id="username"><br>
+        <label for="">Password:</label><br>
+        <input type="password" name="password" id="password"><br>
+        <button type="submit">Submit</button><br>
+    </form>
+
+    
+</body>
+</html>
+
+<?php
+//For Chrome Live Server extension:
+//http://localhost/phpProjects/brocode/
+//http://127.0.0.1:5500/
+
+//Printing:
+    echo "Testing echo statement in PHP!<br>";
+    echo"This is jank<br>";
+//Variables not statically/strongly typed:
+    $name = "Wyatt";
+    $age = 29;
+    $price = 4.99;
+    $online = true;
+    $falsey = false;
+
+    //Same way to print variables:
+    echo "My name is {$name} and my age is {$age}<br>";
+    echo "<h1>My name is $name and my age is $age<br></h1>"; //Interpolation
+    echo '<i>My name is '.$name.' and my age is '.$age.'<br></i>'; //Concatenation
+    echo "The price is \$$price<br>"; //Escape character
+    echo "Is online? $online //Trus is 1!!!<br>";
+    echo "Is online? $falsey :     //False is NOTHING!<br>";
+    echo "Is online? ".($online ? "Yes" : "No")."<br>"; //Ternary operator
+    //Math
+    echo "5 + 2 = ".(5+2)."<br>";
+    echo "5 - 2 = ".(5-2)."<br>";
+    echo "5 * 2 = ".(5*2)."<br>";
+    echo "5 / 2 = ".(5/2)."<br>";
+    echo "5 % 2 = ".(5%2)."<br>";
+    echo "5 ** 2 = ".(5**2)."<br>";
+
+    // $_GET and $_POST
+    echo $_GET['username']."<br>"; //Will print the username from the form
+    echo $_GET['password']."<br>"; //Will print the password from the form
+
+
+
+?>
